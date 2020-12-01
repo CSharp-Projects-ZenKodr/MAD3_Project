351215ba92d16c9c1141e95f98bdc8bc29e4a5ce
MrSkillage <43910132+MrSkillage@users.noreply.github.com>
2020-12-01 17:15:18 +0000

    Edited main menu and added a countdown timer to the game.

---
+++ Chasing Status/Assets/__Scripts/Countdown.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Countdown : MonoBehaviour
+{
+    public float timeStart = 60.0f;
+    [SerializeField]
+    public TextMeshProUGUI textBox;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textBox.text = timeStart.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeStart -= Time.deltaTime;
+        textBox.text = Mathf.Round(timeStart).ToString();
+    }
+}

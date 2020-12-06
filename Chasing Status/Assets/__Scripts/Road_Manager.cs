04bf09ac2664e5c989d1a9106b94f29cd09ef820
MrSkillage <43910132+MrSkillage@users.noreply.github.com>
2020-12-06 18:59:04 +0000

    Change folder hierarchy and add some score and new road prefabs.

--- Chasing Status/Assets/__Scripts/Road_Manager.cs
+++ Chasing Status/Assets/__Scripts/Road_Manager.cs
@@ -7,13 +7,17 @@ public class Road_Manager : MonoBehaviour
     // New array of game objects made up of roadPrefabs
     public GameObject[] roadPrefabs;
 
+    // Checkpoint game object
+    public GameObject checkPoint;
+
     private List<GameObject> activeRoads = new List<GameObject>();
 
     // The z position of the roadPrefabs
     public float zSpawnLoc = 0;
     public float roadLength = 30;
+
     // Number of Roads
-    public int numberOfRoads = 5;
+    public int numberOfRoads = 4;
     // Player transform position 
     public Transform playerTransform;
 
@@ -36,10 +40,20 @@ public class Road_Manager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Gets players position z and calculates if less then the all the roads lengths
         if (playerTransform.position.z - 35 > zSpawnLoc-(numberOfRoads * roadLength))
         {
-            SpawnTile(Random.Range(0, roadPrefabs.Length));
-            DeleteRoad();
+            // Road size is 30 so % 900 will always be a consistent messure
+            if (zSpawnLoc % 900 == 0)
+            {
+                
+                SpawnCheckpoint(); // Spawns a checkpoint prefab
+                DeleteRoad(); // Deletes old roads
+            } else
+            {
+                SpawnTile(Random.Range(0, roadPrefabs.Length)); // Spawns a random road prefab from a list
+                DeleteRoad(); // Deletes old roads
+            }
         }
     }
 
@@ -47,6 +61,13 @@ public class Road_Manager : MonoBehaviour
     {
         GameObject gameObj = Instantiate(roadPrefabs[roadIdex], transform.forward * zSpawnLoc, transform.rotation);
         activeRoads.Add(gameObj);
+        zSpawnLoc += roadLength;
+    }
+
+    public void SpawnCheckpoint()
+    {
+        GameObject cp = Instantiate(checkPoint, transform.forward * zSpawnLoc, transform.rotation);
+        activeRoads.Add(cp);
         zSpawnLoc += roadLength;
     }
 
---
+++ Chasing Status/Assets/__Scripts/Score.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class Score : MonoBehaviour
+{
+    public TextMeshProUGUI score;
+
+    public float scoreCount;
+    public float pointsPerSecond;
+
+    private void Update()
+    {
+        scoreCount += pointsPerSecond * Time.deltaTime;
+        score.text = "Score: " + Mathf.RoundToInt(scoreCount);
+    }
+}
